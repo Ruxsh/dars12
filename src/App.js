07010bf0bb42ee67d8a474465c39ee4ddddaf758
Black-monster1 <otabekruhshona@gmail.com>
2024-03-02 19:31:32 +0500

    Add commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import monitor from './monitor.png';
+import monitor from './monitor.png'
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <header>
       <p>solo</p>
+      <img src = {monitor}></img>
       <p>solo</p>
      </header>
     </div>
